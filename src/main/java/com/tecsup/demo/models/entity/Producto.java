8651d96895c3d668bbbccccb8f07f104772d3bdb
Mauricio-DelVillar-Canchari <User@192.168.1.9>
2021-11-18 21:59:34 -0500

    Union de la carpteta de Ronal y Alonso

---
+++ src/main/java/com/tecsup/demo/models/entity/Producto.java
@@ -0,0 +1,70 @@
+package com.tecsup.demo.models.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "producto")
+public class Producto {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	@NotNull
+	private String nombre;
+	@NotNull
+	private int stock;
+	@NotNull
+	private double precio;
+	@NotNull
+	private String descripcion;
+	
+	public Producto(int id, @NotNull String nombre, @NotNull int stock, @NotNull double precio,
+			@NotNull String descripcion) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.stock = stock;
+		this.precio = precio;
+		this.descripcion = descripcion;
+	}
+	
+	public Producto() {
+		
+	}
+
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public int getStock() {
+		return stock;
+	}
+	public void setStock(int stock) {
+		this.stock = stock;
+	}
+	public double getPrecio() {
+		return precio;
+	}
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	
+}
