11ded5d934107c5d8beb7e6c65f5d62bebac029d
Mauricio-DelVillar-Canchari <User@192.168.1.9>
2021-11-24 20:43:39 -0500

    Prueba

--- src/main/java/com/tecsup/demo/controllers/InicioController.java
+++ src/main/java/com/tecsup/demo/controllers/InicioController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class InicioController {
 	
 	// Podriamos inculir aca la ruleta de los productos
+	// Mauricio Paso por aca
 	@GetMapping("/")
 	public String inicio() {
 		return "inicio";
