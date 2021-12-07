5a40cd2123a3c796d1bab7686515009364d1c40c
YesicaLope <yesica.lope@tecsup.edu.pe>
2021-12-07 03:11:27 -0500

    update de la clase InicioController

--- src/main/java/com/tecsup/demo/controllers/InicioController.java
+++ src/main/java/com/tecsup/demo/controllers/InicioController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class InicioController {
 	
+	// Podriamos inculir aca la ruleta de los productos
 	@GetMapping("/")
 	public String inicio() {
 		return "inicio";
